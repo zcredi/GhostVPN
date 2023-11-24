6d494967563453aa501625d779240eaa1c3a6698
Vladislav <107634092+zcredi@users.noreply.github.com>
2023-11-24 17:42:05 +0300

    Initial Commit

---
+++ GhostVPN/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GhostVPN
+//
+//  Created by Владислав on 24.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
